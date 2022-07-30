134ae69ada15113e8caab232067982f8101adba4
DhanushM12 <dhanushmukhi@gmail.com>
2022-07-30 13:21:30 +0530

    added reducers and store

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducers from './reducers/index';
+
+let store = createStore(reducers);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+ <Provider store={store}>
     <App />
-  </React.StrictMode>
+ </Provider>
+    
 );
